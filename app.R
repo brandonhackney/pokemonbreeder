0a63fcea19f6bbac2a22fcf965f92fffcc395f13
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2026-01-26 22:32:27 -0800

    Add dead UI element

--- app.R
+++ app.R
@@ -7,15 +7,21 @@ eggs <- getEggGroups()
 myList <- eggs$Number[as.logical(eggs$Dragon)]
 # A function that organizes the UI elements of the shiny app	
 uiF <- page_sidebar(
-	title = "title panel",
-	sidebar = sidebar("Bulbasaur"),
+	title = "Pokemon Gen2 Egg Group Analyzer",
+	sidebar = sidebar(
+		helpText("List all possible mates for the selected Pokemon"),
+		selectInput(
+			"Dropdown",
+			label = "Select a Pokemon",
+			choices = eggs$Name
+		)
+	),
 	
 			layout_column_wrap(
 				getCard(myList[1],eggs),
 				getCard(myList[2],eggs),
 				getCard(myList[3],eggs)
 			)
-	
 )
 
 # A function that runs the actual code
